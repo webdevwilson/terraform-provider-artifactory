b9d224da30c23762ba883c39d5b073f9b72cb956
Kerry Wilson <kerry@allthingswilson.com>
2017-09-25 22:16:44 -0500

    Acceptance tests passing on clean install

--- artifactory/resource_virtual_repository_test.go
+++ artifactory/resource_virtual_repository_test.go
@@ -12,15 +12,16 @@ resource "artifactory_virtual_repository" "foobar" {
 }`
 
 func TestAccVirtualRepository_basic(t *testing.T) {
+	resourceName := "artifactory_virtual_repository.foobar"
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
-		CheckDestroy: testAccCheckRepositoryDestroy("artifactory_virtual_repository.foobar"),
+		CheckDestroy: testAccCheckRepositoryDestroy(resourceName),
 		Providers:    testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccVirtualRepository_basic,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "key", "acctest-virtual-basic"),
+					resource.TestCheckResourceAttr(resourceName, "key", "acctest-virtual-basic"),
 				),
 			},
 		},
@@ -28,8 +29,8 @@ func TestAccVirtualRepository_basic(t *testing.T) {
 }
 
 const testAccVirtualRepository_full = `
-resource "artifactory_remote_repository" "npm-public" {
-  key               = "registry.npmjs.org"
+resource "artifactory_remote_repository" "npm_public" {
+  key               = "acctest-virtual-npm-private"
   package_type      = "npm"
   description       = "Proxy public npm registry"
   repo_layout_ref   = "npm-default"
@@ -39,12 +40,13 @@ resource "artifactory_remote_repository" "npm-public" {
   ]
 }
 
-resource "artifactory_local_repository" "npm-local" {
-	key 	     = "acctest-virtual-npm-local"
-	package_type = "npm"
+resource "artifactory_local_repository" "npm_private" {
+	key 	     	= "acctest-virtual-npm-private"
+	package_type 	= "npm"
+	repo_layout_ref = "npm-default"
 }
 
-resource "artifactory_virtual_repository" "foobar" {
+resource "artifactory_virtual_repository" "npm" {
     key                                                = "acctest-virtual-full"
     package_type                                       = "npm"
 	description                                        = "desc"
@@ -56,37 +58,36 @@ resource "artifactory_virtual_repository" "foobar" {
     artifactory_requests_can_retrieve_remote_artifacts = false
 	key_pair                                           = "keypair"
     pom_repository_references_cleanup_policy           = "discard_any_reference"
-    default_deployment_repo                            = "acctest-virtual-npm-local"
+    default_deployment_repo                            = "${artifactory_local_repository.npm_private.key}"
 	debian_trivial_layout                              = false
-    repositories                                       = ["registry.npmjs.org"]
-	depends_on										   = [
-		"artifactory_remote_repository.npm-public",
-		"artifactory_local_repository.npm-local"
+    repositories                                       = [
+		"${artifactory_remote_repository.npm_public.key}",
+		"${artifactory_local_repository.npm_private.key}"
 	]
 }`
 
 func TestAccVirtualRepository_full(t *testing.T) {
+	resourceName := "artifactory_virtual_repository.npm"
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
-		CheckDestroy: testAccCheckRepositoryDestroy("artifactory_virtual_repository.foobar"),
+		CheckDestroy: testAccCheckRepositoryDestroy(resourceName),
 		Providers:    testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccVirtualRepository_full,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "key", "acctest-virtual-full"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "package_type", "npm"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "description", "desc"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "notes", "the notes"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "includes_pattern", "**/*"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "excludes_pattern", "**/*.tgz"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "repo_layout_ref", "npm-default"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "artifactory_requests_can_retrieve_remote_artifacts", "false"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "key_pair", "keypair"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "pom_repository_references_cleanup_policy", "discard_any_reference"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "default_deployment_repo", "npm-local"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "repositories.#", "1"),
-					resource.TestCheckResourceAttr("artifactory_virtual_repository.foobar", "repositories.619022263", "registry.npmjs.org"),
+					resource.TestCheckResourceAttr(resourceName, "key", "acctest-virtual-full"),
+					resource.TestCheckResourceAttr(resourceName, "package_type", "npm"),
+					resource.TestCheckResourceAttr(resourceName, "description", "desc"),
+					resource.TestCheckResourceAttr(resourceName, "notes", "the notes"),
+					resource.TestCheckResourceAttr(resourceName, "includes_pattern", "**/*"),
+					resource.TestCheckResourceAttr(resourceName, "excludes_pattern", "**/*.tgz"),
+					resource.TestCheckResourceAttr(resourceName, "repo_layout_ref", "npm-default"),
+					resource.TestCheckResourceAttr(resourceName, "artifactory_requests_can_retrieve_remote_artifacts", "false"),
+					resource.TestCheckResourceAttr(resourceName, "key_pair", "keypair"),
+					resource.TestCheckResourceAttr(resourceName, "pom_repository_references_cleanup_policy", "discard_any_reference"),
+					resource.TestCheckResourceAttr(resourceName, "default_deployment_repo", "acctest-virtual-npm-private"),
+					resource.TestCheckResourceAttr(resourceName, "repositories.#", "2"),
 				),
 			},
 		},
